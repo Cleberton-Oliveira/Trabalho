7389281b336c08b15e6a38a54ab3fe943e39b8ab
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2018-03-28 16:29:28 -0300

    Apagando coisas desnecessarias

--- js/function-trabalho.js
+++ js/function-trabalho.js
@@ -1,8 +1,12 @@
+
+//----------------------------
 var hex = null;
-	var dec = null;
-	var bin = null;
-	var ascii = null;
+var dec = null;
+var bin = null;
+var ascii = null;
+//----------------------------	
 	function updateValue(value, exclude) {
+	  
 	  if (exclude != hex) {
 	    hex.value = value.toString(16);
 	  }
@@ -18,24 +22,48 @@ var hex = null;
 	    } catch(e) {}
 	  }
 	}
+
+//----------------------------
+	
 	window.addEventListener('load', function() {
+	 
+
+
 	  hex = document.querySelector('.hex.input');
 	  hex.value = 0;
+	  
 	  hex.addEventListener('input', function() {
 	    updateValue(parseInt(hex.value, 16), hex);
+	  
+
 	  }, false);
+
+
+
+
 
 	  dec = document.querySelector('.dec.input');
 	  dec.value = 0;
+	  
 	  dec.addEventListener('input', function() {
 	    updateValue(parseInt(dec.value, 10), dec);
 	  }, false);
+
+
+
+
 	  bin = document.querySelector('.bin.input');
 	  bin.value = 0;
+	  
 	  bin.addEventListener('input', function() {
 	    updateValue(parseInt(bin.value, 2), bin);
 	  }, false);
 
+
+
+
+
 	  ascii = document.querySelector('.ascii.input');
 }, false);
 
+//----------------------------
